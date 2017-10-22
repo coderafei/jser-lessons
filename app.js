995d99fdb820759558df4fb0c8ccc86730c326d8
coderafei <i@feit.me>
2017-10-22 21:18:41 +0800

    Add signin and signup page

--- app.js
+++ app.js
@@ -7,6 +7,47 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 var moments = [];
+var users = [];
+
+app.post('/signup', function (req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  var isExists = false;
+  users.forEach(function (user) {
+    if (user.username === username) {
+      isExists = true;
+    }
+  });
+
+  if (isExists) {
+    res.json({message: '用户名已经存在'});
+    return;
+  }
+
+  users.push({username: username, password: password});
+
+  res.json({message: '用户创建成功'});
+});
+
+app.post('/signin', function (req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  var isPasswrodCorrect = false;
+
+  users.forEach(function (user) {
+    if (user.username === username && user.password === password) {
+      isPasswrodCorrect = true;
+    }
+  });
+
+  if (isPasswrodCorrect) {
+    res.json({message: '登录成功'});
+  } else {
+    res.json({message: '登录失败'});
+  }
+});
 
 app.post('/createMoment', function (req, res) {
   var name = req.body.name;
